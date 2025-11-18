3bcf591ab6721aa73870628d546e99657834f09f
lamnh0404 <150221983+CaoNhatLam@users.noreply.github.com>
2025-11-18 23:37:22 +0700

    add req.session

--- src/routes/homepage/homepage.js
+++ src/routes/homepage/homepage.js
@@ -3,7 +3,7 @@ import express from 'express'
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.render('homepage/homepage.ejs', { title: 'Welcome to the Homepage' })
+    res.render('homepage/homepage.ejs', { title: 'Welcome to the Homepage', user : req.session.user } )
 }
 )
 
