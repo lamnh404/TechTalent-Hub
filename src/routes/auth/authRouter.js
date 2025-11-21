9ab84ca29cffd9945765737272b8a2a9153a416c
Anh Huy <ptanhhuy9105@gmail.com>
2025-11-21 21:27:27 +0700

    Hello Bros

--- src/routes/auth/authRouter.js
+++ src/routes/auth/authRouter.js
@@ -11,6 +11,13 @@ router.get('/register', (req, res) => {
     res.render('auth/register.ejs', { title: 'Register' })
 })
 
+router.get('/setup-seeker', (req, res) => {
+    res.render('auth/setup-seeker.ejs', { title: 'Seeker Profile Setup' })
+})
+router.get('/setup-company', (req, res) => {
+    res.render('auth/setup-company.ejs', { title: 'Company Profile Setup' })
+})
+
 router.post('/login', authController.login)
 router.post('/register', authController.register)
 export const authRouter = router
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import { homepageRouter } from './homepage/homepage'
 import { authRouter } from './auth/authRouter'
+import { pagesRouter } from './pages/pages'
 
 
 const router = express.Router()
@@ -9,5 +10,6 @@ router.use('/', homepageRouter)
 
 router.use('/', authRouter)
 
+router.use('/', pagesRouter)
 
 export const API = router
