81f5aaf8529af4ab7362a9f8d44c2f50b0d70df1
Phạm Trương Anh Huy <ptanhhuy9105@gmail.com>
2025-11-26 15:38:45 +0700

    Update UI

--- src/routes/homepage/homepage.js
+++ src/routes/homepage/homepage.js
@@ -3,15 +3,15 @@ import express from 'express'
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.render('homepage/homepage.ejs', { title: 'Welcome to the Homepage', user : req.session.user } )
+    res.render('homepage/homepage.ejs', { title: 'Welcome to the Homepage', user : req.session.user} )
 })
 
 router.get('/jobs', (req, res) => {
-    res.render('homepage/job-listing.ejs', { title: 'Job Listings', user : req.session.user } )
+    res.render('homepage/job-listing.ejs', { title: 'Job Listings', user : req.session.user} )
 })
 
 router.get('/jobs/:id', (req, res) => {
     const jobId = req.params.id;
-    res.render('homepage/job-detail.ejs', { title: 'Job Detail', user : req.session.user, jobId } )
+    res.render('homepage/job-detail.ejs', { title: 'Job Detail', user : req.session.user, jobId} )
 })
 export const homepageRouter = router
